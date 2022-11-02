485224c4715105ce7048858f2645983f738c9849
Ian Lomibao <ilomibao@ucsd.edu>
2022-11-02 01:56:54 -0700

    reworked JS for quantity buttons w/ jQuery

--- assets/product-page-jquery.js
+++ assets/product-page-jquery.js
@@ -1,49 +1,25 @@
 $(document).ready(function() {
     console.log('product-page-jquery.js loaded in');
 
-    const titleColor = $("#product-title-color-choice"),
-            mainImage = $("#main-image");
+    const update_option_header = function(optionContainer, optionTitle, optionInput) {
+        optionTitle.text(optionInput.attr("value"));
+        optionContainer.find("input.selected").removeClass("selected");
+        optionInput.addClass("selected")
+    }
 
-    const colorSelectors = $("#swatch .color-btn"),
-            carouselButtons = $("#img-horizontal-carousel .carousel-btn");
-
-    const update_product_choice = function(colorInput, carouselButton) 
-    {
-        update_color_text(colorInput);
-        update_main_image(carouselButton);
-    };
-
-    const update_color_text = function(colorInput) {
-        titleColor.text(colorInput.attr("value"));
-        $(".color-btn--selected").removeClass("color-btn--selected");
-        colorInput.addClass("color-btn--selected");
-    };
-
-    const update_main_image = function(carouselImage) {
-        // const carouselImage = carouselButton.children("img");
-        mainImage.attr({
-            src: carouselImage.attr("src"),
-            alt: carouselImage.attr("alt")
+    const update_main_image = function(image) {
+        $("#main-image").attr({
+            src: image.attr("src"),
+            alt: image.attr("alt")
         });
     };
 
-    // for (var i = 0; i < colorSelectors.length; i++) 
-    // {
-    //     const colorInput = colorSelectors.eq(i),
-    //           carouselButton = carouselButtons.eq(i);
-    //     // colorInput.click(() => {update_product_choice(colorInput, carouselButton)});   
-    //     // carouselButton.click(() => {update_product_choice(colorInput, carouselButton)});
-    //     colorInput.on( "click", )
-    // };
-
-    // if image or swatch is attached to a variant
-    //  update both color text and image
     $("#img-horizontal-carousel").on( "click", "button", function() {
         update_main_image($(this).children("img"));
     });
 
     $("#swatch").on( "click", "input", function() {
-        update_color_text($(this));
+        update_option_header($("#swatch"), $("#product-title-color-choice"), $(this));
         const variantImage = $(".carousel-image[data-variants~="+$(this).attr("data-option-name")+"]");
         if(variantImage != null) {
             update_main_image(variantImage);
@@ -59,14 +35,15 @@ $(document).ready(function() {
         }
         else 
         {
-            const newQty = Number(qtyValue.val()) + int;
+            const newQty = Number(qtyValue.val()) + Number(int);
             if( newQty >= 1 ) {
                 qtyValue.val(newQty);
             }
         }
     };
 
-    $("#minus").click(() => {update_qty(-1)});
-    $("#plus").click(() => {update_qty(1)});
+    $(".qty-adjust").on("click", function() {
+        update_qty($(this).attr("qty"));
+    })
     
 });
